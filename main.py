e71af5a767f3811042488af96868407068789ca0
MatheusOCC <escrevaparamatheus@gmail.com>
2023-06-03 12:19:58 -0300

    recebe conjuunto de tarefas e transforma em lista de listas

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-def main():
+def cria_cabecalho():
     st.title('Projeto Escalonador')
     col1,col2 = st.columns(2)
     with col1:
@@ -21,6 +21,34 @@ def main():
             '''
         )
     st.divider()
+
+
+def get_conjunto_tarefas() -> list:
+    conjunto_tarefas = st.text_input(
+        'Escreva o Conjunto de Tarefas a ser escalonado',
+        help='''
+        Exemplo: (1,2);(1,4);(1,6);(2,5). 
+        Parênteses e espaços são desconsiderados.
+        '''
+    )
+    try:
+        conjunto_tarefas = conjunto_tarefas.split(';')
+    except:
+        st.warning(
+            'Por favor, adeque o conjunto de tarefas ao formato esperado.'
+        )
+    tarefas = []
+    for tarefa in conjunto_tarefas:
+        tarefas.append(
+            tarefa.replace('(','').replace(')','').replace(' ','').split(',')
+        )
+    st.info(tarefas)
+    return tarefas
+
+
+def main():
+    cria_cabecalho()
+    tarefas = get_conjunto_tarefas()
 
 
 if __name__ == "__main__":
